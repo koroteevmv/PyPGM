6374b049858b9971cdafb50f1d498563054a0614
sejros <sejros@sejros-main.(none)>
2013-03-01 12:05:34 +0400

    v 0.03 fixed

--- factors.py
+++ factors.py
@@ -25,11 +25,11 @@ class Variable:
         self.factors=[]
         self.parents=[]
         self.childs=[]
-        if  (type(a)==int):     # cardinality passed
+        if  (type(a)==int):             # cardinality passed
             self.card = a
             for i in range(a):
                 self.value.append(i)
-        elif(type(a)==list):    # values list passed
+        elif(type(a)==list):            # values list passed
             self.value = a
             self.card = len(a)
         self.PD=None
@@ -43,19 +43,15 @@ class Variable:
     def __repr__(self):
         return self.name
 
-    def get_PD(self):
-        # calculates probability distribution w/ no conditions
-        if self.PD==None:
-            # find a factor contains this var as cons
+    def get_PD(self):                                       # calculates probability distribution w/ no conditions
+        if self.PD==None:                                   # find a factor contains this var as cons
             f = None
             for factor in self.factors:
                 if factor.cons == [self]:
                     f = factor
-            # marginalize all other vars from this factor
-            for v in list(set(f.var) - set([self])):
+            for v in list(set(f.var) - set([self])):        # marginalize all other vars from this factor
                 f = f - v
-            # and extract PD from this reduced factor's CPD
-            self.PD = f.CPDs
+            self.PD = f.CPDs                                # and extract PD from this reduced factor's CPD
         return self.PD
 
 class BinaryVariable(Variable):
@@ -85,12 +81,12 @@ class Factor:
         self.cond=cond
         self.var=self.cons + self.cond
         for i in self.cons:
-            i.factors.append(self)      # adds self to each variable' factor list
-            self.card.append(i.card)    # builds factor's cardinality list from var's cardinalities
-        for i in self.cond:         # in case of conditioning
-            self.card.append(i.card)    # continues factor'a cardinality list
-            i.factors.append(self)      # adds self to variables' factor list
-            for j in self.cons:     # builds variables' hierarchy
+            i.factors.append(self)              # adds self to each variable' factor list
+            self.card.append(i.card)            # builds factor's cardinality list from var's cardinalities
+        for i in self.cond:                     # in case of conditioning
+            self.card.append(i.card)            # continues factor'a cardinality list
+            i.factors.append(self)              # adds self to variables' factor list
+            for j in self.cons:                 # builds variables' hierarchy
                 i.childs.append(j)
                 j.parents.append(i)
         # counts cumulative cardinality
@@ -189,12 +185,13 @@ class Factor:
         for i in range(res.pcard[0]):
             res.CPDs.append(0)
         print res.cons, res.cond
-        print self.cons, other.cons
+        print self.var, other.cons
+        print mapX, mapY
         for i in range(len(self.CPDs)):
-            assX = res.ass(i, mapX)
-            assY = res.ass(i, mapY)
+            assX = self.ass(i, mapX)
+            assY = self.ass(i, mapY)
             print assX, assY
-            print self.CPDs[res.ass2index(assX)]
+            print self.CPDs[self.ass2index(assX)],
             print other.CPDs[other.ass2index(assY)]
             res.CPDs[i] = self.CPDs[self.ass2index(assX)] / other.CPDs[other.ass2index(assY)]
         return res
@@ -211,31 +208,32 @@ F4 = Factor(name='A|E,B', cons=[A], cond=[E, B],
         CPDs=[1, 0, 0.01, 0.99, 0.02, 0.98, 0, 1]);
 
 F6=F1*F2
+print F1
 print F6
 print F6/F1
 
-##F5 = F3 - E
-##F5.name = 'R'
-##print F5
-##F6 = F5 * F1
-##F6.name = 'R,E'
-##print F6
-##F7 = F6/F5
-##F7.name = 'E|R'
-##print F7
+F5 = F3 - E
+F5.name = 'R'
+print F5
+F6 = F5 * F1
+F6.name = 'R,E'
+print F6
+F7 = F6/F5
+F7.name = 'E|R'
+print F7
 
 ##print F3-E
 ##print F3.ass2index(F3.index2ass(3))
 ##for i in range(F3.pcard[0]):
 ##    print i, F3.index2ass(i), F3.ass2index(F3.index2ass(i))
 
-##C = Variable('Cancer', ['yes', 'no'])
-##T = Variable('Test', ['pos', 'neg'])
-##
-##F11 = Factor(name='C', cons=[C], CPDs=[0.0001, 0.9999])
-##F12 = Factor(name='T|C', cons=[T], cond=[C], CPDs=[0.9, 0.1, 0.2, 0.8])
-##F14 = (F12 - C) * F11
-##F14.name='C,T'
-##F15 = F14/(F12 - C)
-##F15.name='C|T'
+C = Variable('Cancer', ['yes', 'no'])
+T = Variable('Test', ['pos', 'neg'])
 
+F11 = Factor(name='C', cons=[C], CPDs=[0.0001, 0.9999])
+F12 = Factor(name='T|C', cons=[T], cond=[C], CPDs=[0.9, 0.1, 0.2, 0.8])
+F14 = (F12 - C) * F11
+F14.name='C,T'
+F15 = F14/(F12 - C)
+F15.name='C|T'
+print F15
